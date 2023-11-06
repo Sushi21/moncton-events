0c32e31d2f7f93aa92fac2a8fd80de565b8d3461
YannickDufils <yannick.dufils@procedureflow.com>
2023-11-06 13:37:41 -0400

    go to top

--- main.js
+++ main.js
@@ -119,3 +119,19 @@ document.getElementById('venueFilter').addEventListener('change', () => {
 });
 
 displayEvents({});
+
+// Get the button
+let mybutton = document.getElementById('goTop');
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = 'block';
+  } else {
+    mybutton.style.display = 'none';
+  }
+}
